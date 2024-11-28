461e8947ddc19ae6f1c5f9d102cf6a50474ed94e
Nesrullayev <senan.nesrullayevv@gmail.com>
2024-11-28 10:56:59 +0400

    Adding AdoNet

---
+++ Ado.Net/Ado.Net/Program.cs
@@ -0,0 +1,29 @@
+﻿using Ado.Net.Service;
+
+namespace Ado.Net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           StudentService studentService = new StudentService();
+            //Data Insert Elemek ucun
+            studentService.Create(new() { Name = "Student2", Age = 18 });
+            //Datalarin hamisini oxumaq ucun
+            foreach (var student in studentService.GetAll())
+            {
+                Console.WriteLine(student);
+            }
+            //Oturulen Id ye esasen tapmaq ucun
+            Console.WriteLine(studentService.GetById(2));
+            //ID nen silinmek ucun 
+            studentService.DeleteByID(2);
+            //Update ucun
+            studentService.Update(1, new() { Name = "Senan", Age = 18 });
+            //Function cagirmaq ucun
+            Console.WriteLine(studentService.GetDataCount());
+            //Procedure isletmek
+            studentService.GetDataWithAge(18);
+        }
+    }
+}
---
+++ Ado.Net/Ado.Net/Service/StudentService.cs
@@ -0,0 +1,158 @@
+﻿using Ado.Net.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado.Net.Service
+{
+    public class StudentService
+    {
+        [Obsolete]
+        public void Create(Student student)
+        {
+            string connectionString = "Server=.; Database=PB502; User Id=sa; Password=Senan123@;";
+            using SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            string Query = "INSERT INTO STUDENTS VALUES(@Name,@Age)";
+            SqlCommand sqlCommand = new SqlCommand(Query, sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@Name", student.Name);
+            sqlCommand.Parameters.AddWithValue("@Age", student.Age);
+            //Update,Delete,Insert(ExecuteNonQuery ishlenir)
+            //Eger Execute 0 dan boyukdurse ugurludur eks halda ugursuz
+            sqlCommand.ExecuteNonQuery();
+        }
+        public List<Student> GetAll()
+        {
+            List<Student> studentsList = new List<Student>();
+            string connectionString = "Server=.; Database=PB502; User Id=sa; Password=Senan123@;";
+            using SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            string Query = "Select * from Students";
+            SqlCommand sqlCommand = new SqlCommand(Query, sqlConnection);
+            //Yalniz Read emelyatlarinda reader ishledirik
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            //Eger hasrows true gelirse table doludur eks halda bosdur
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    int _id = (int)reader.GetValue(0);
+                    string _name = (string)reader.GetValue(1);
+                    int _age = (int)reader.GetValue(2);
+                    studentsList.Add(new Student { ID = _id, Name = _name, Age = _age });
+                }
+                return studentsList;
+            }
+            return studentsList;
+        }
+        public Student GetById(int ID)
+        {
+            string connectionString = "Server=.; Database=PB502; User Id=sa; Password=Senan123@;";
+            using SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            string Query = "Select * from Students WHere ID=@id";
+            SqlCommand sqlCommand = new SqlCommand(Query, sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@id", ID);
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            Student newStudent = null;
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    int _id = (int)reader.GetValue(0);
+                    string _name = (string)reader.GetValue(1);
+                    int _age = (int)reader.GetValue(2);
+                    newStudent = new() { ID = _id, Name = _name, Age = _age };
+                }
+                return newStudent;
+            }
+            return newStudent;
+        }
+        public void DeleteByID(int ID)
+        {
+            string connectionString = "Server=.; Database=PB502; User Id=sa; Password=Senan123@;";
+            using SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            string Query = "Delete from Students WHere ID=@id";
+            SqlCommand sqlCommand = new SqlCommand(Query, sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@id", ID);
+            if (sqlCommand.ExecuteNonQuery() > 0)
+                Console.WriteLine("Deleted");
+            else Console.WriteLine("Doesn't found");
+        }
+        public void Update(int id, Student student)
+        {
+            string connectionString = "Server=.; Database=PB502; User Id=sa; Password=Senan123@;";
+            using SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            string Query = "Update STUDENTS SET Name=@Name,AGE = @Age WHERE ID = @ID";
+            SqlCommand sqlCommand = new SqlCommand(Query, sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@ID", id);
+            sqlCommand.Parameters.AddWithValue("@Name", student.Name);
+            sqlCommand.Parameters.AddWithValue("@Age", student.Age);
+            sqlCommand.ExecuteNonQuery();
+            if (sqlCommand.ExecuteNonQuery() > 0)
+                Console.WriteLine("Updated");
+            else Console.WriteLine("Not Succesfull");
+        }
+        public string GetAllWithView()
+        {
+            string connectionString = "Server=.; Database=PB502; User Id=sa; Password=Senan123@;";
+            using SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            string Query = "Create view GetNameWithID AS Select S.Name from Students S Where ID=1";
+            SqlCommand sqlCommand = new SqlCommand(Query, sqlConnection);
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            string _name = null;
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    _name = (string)reader.GetValue(0);
+                }
+                return _name;
+            }
+            return null;
+        }
+        public int GetDataCount()
+        {
+            string connectionString = "Server=.; Database=PB502; User Id=sa; Password=Senan123@;";
+            using SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            string Query = "Select dbo.GetCount()";
+            SqlCommand sqlCommand = new SqlCommand(Query, sqlConnection);
+            int result = (int)sqlCommand.ExecuteScalar();
+            return result;
+        }
+        public void GetDataWithAge(int age)
+        {
+            List<Student> StuList = new();  
+            string connectionString = "Server=.; Database=PB502; User Id=sa; Password=Senan123@;";
+            using SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            string Query = "GetData";
+            SqlCommand sqlCommand = new SqlCommand(Query, sqlConnection);
+            sqlCommand.CommandType=System.Data.CommandType.StoredProcedure;
+            sqlCommand.Parameters.AddWithValue("@Age",age);
+            SqlDataReader reader=sqlCommand.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    int _id = (int)reader.GetValue(0);
+                    string _name = (string)reader.GetValue(1);
+                    int _age = (int)reader.GetValue(2);
+                    StuList.Add(new() { ID = _id, Name = _name, Age = _age });
+                }
+                foreach (Student student in StuList)
+                {
+                    Console.Write(student.Name );
+                }
+            }
+            else Console.WriteLine("Not Succesfull");
+        }
+    }
+}
